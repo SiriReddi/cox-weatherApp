06eba1b0d46924200d0b1652e098e0e62e16ccb1
sirisha Basireddy <54509662+SiriReddi@users.noreply.github.com>
2021-05-06 11:55:00 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,79 @@
 import React from 'react';
-import { render, screen, act } from '@testing-library/react';
+import {
+  render,
+  cleanup,
+  act,
+  fireEvent,
+  screen,
+} from '@testing-library/react';
+
+import Form from './components/Form.jsx';
+
 import App from './App';
+
+const mockOnSubmit = jest.fn();
+
+describe('Form', () => {
+  describe('with valid inputs', () => {
+    afterEach(cleanup);
+
+    it('calls the onSubmit function', async () => {
+      await render(<Form onSubmit={mockOnSubmit('33.8001,-118.2962')} />);
+
+      const longitude = screen.getByPlaceholderText('longitude');
+      const latitude = screen.getByPlaceholderText('longitude');
+
+      fireEvent.input(longitude);
+      fireEvent.input(latitude);
+
+      const button = screen.getByRole('button');
+      fireEvent.submit(button);
+
+      expect(mockOnSubmit).toBeCalledWith('33.8001,-118.2962');
+    });
+  });
+});
+
+async function fetchWeather() {
+  const res = await fetch('https://api.weather.gov/points/33.8001,-118.2962');
+  const json = await res.json();
+
+  return json;
+}
+describe('App', () => {
+  test('works', async () => {
+    const fetchMock = jest
+      .spyOn(global, 'fetch')
+      .mockImplementation(() =>
+        Promise.resolve({ json: () => Promise.resolve([]) })
+      );
+
+    render(<App />);
+    const json = await fetchWeather();
+    expect(fetchMock).toHaveBeenCalledWith(
+      'https://api.weather.gov/points/33.8001,-118.2962'
+    );
+
+    expect(Array.isArray(json)).toEqual(true);
+    expect(json.length).toEqual(0);
+  });
+});
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
-    json: () =>
-      Promise.resolve({
-        weatherData: 'https://api.weather.gov/points/33.8001,-118.2962',
-        temperature: '68°F',
-        city: 'Lomita',
-        state: 'CA',
-      }),
+    data: () => ({
+      temperature: '68°F',
+      city: 'alpharetta',
+      state: 'CA',
+    }),
   })
 );
-
-describe('App', () => {
-  it('loads the weather data on mount', async () => {
+describe('after fetching the data successfully', () => {
+  it('handles the state successfully', async () => {
     await act(async () => render(<App />));
+
     screen.getByText('Weather App');
     expect(screen.getByText('Weather App')).toBeInTheDocument();
   });
 });
+
